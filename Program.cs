c99ae317e4e7656fb3132622300b6a2bca25487c
mazylol <landonp95@gmail.com>
2023-04-25 18:23:12 -0400

    base bot

--- Program.cs
+++ Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DotNetEnv;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+namespace FM6
+{
+    internal abstract class Program
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Env.TraversePath().Load();
+            var discordToken = Env.GetString("DEV_TOKEN");
+            var guildId = Convert.ToUInt64(Env.GetString("GUILD_ID"));
+
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = discordToken,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged,
+                MinimumLogLevel = LogLevel.Debug
+            });
+
+            discord.Ready += DiscordReady;
+
+            var slash = discord.UseSlashCommands();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task DiscordReady(DiscordClient client, ReadyEventArgs e)
+        {
+            await client.UpdateStatusAsync(new DiscordActivity($"Protecting {client.Guilds.Count} servers"),
+                UserStatus.Online);
+        }
+    }
+}
